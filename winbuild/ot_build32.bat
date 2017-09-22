05added1250963029940ba6c04d61ab9fc71f948
Brano Kemen <cameni@outerra.com>
2017-09-22 08:48:30 +0200

    ot build settings

---
+++ winbuild/ot_build32.bat
@@ -0,0 +1,2 @@
+call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\bin\vcvars32.bat"
+nmake /f Makefile.vc mode=static VC=14 RTLIBCFG=static MACHINE=x86
---
+++ winbuild/ot_build64.bat
@@ -0,0 +1,2 @@
+call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\bin\amd64\vcvars64.bat "
+nmake /f Makefile.vc mode=static VC=14 RTLIBCFG=static MACHINE=x64
